19ac38fdef3caad6ff3096840b7bf082f1144c28
AssafAlon1 <alon.assaf@campus.technion.ac.il>
2021-06-17 10:14:58 +0300

    17/06/2021 morning

--- main.cpp
+++ main.cpp
@@ -16,6 +16,8 @@ using mtm::MEDIC;
 using mtm::SOLDIER;
 using mtm::POWERLIFTERS;
 using mtm::CROSSFITTERS;
+using mtm::Team;
+
 
 using mtm::Board; // Private
 
@@ -67,8 +69,8 @@ void basicTest1()
     cout << "Running basicTest1... ";
     std::shared_ptr<Character> sniper1 = Game::makeCharacter(SNIPER, POWERLIFTERS, 1, 4, 4, 4);
     std::shared_ptr<Character> sniper2 = Game::makeCharacter(SNIPER, CROSSFITTERS, 1, 4, 4, 4);
-    assert(sniper1->getAscii() == 'S'); // Private
-    assert(sniper2->getAscii() == 's'); // Private
+    assert(sniper1->getAscii() == 'N'); // Private
+    assert(sniper2->getAscii() == 'n'); // Private
 
 
     Game game1 = Game(4, 4);
@@ -76,17 +78,31 @@ void basicTest1()
     game1.addCharacter(co, sniper1);
     game1.addCharacter(GridPoint(1,3), sniper1);
     assert(game1.isOver());
+    Team winning_team = CROSSFITTERS;
+    assert(game1.isOver(&winning_team));
+    assert(winning_team == POWERLIFTERS);
     game1.addCharacter(GridPoint(0,1), sniper2);
     assert(!(game1.isOver()));
     
-    try { game1.addCharacter(GridPoint(-1,0), sniper1); }
+    try {
+        game1.addCharacter(GridPoint(-1,0), sniper1);
+
+    }
     catch (mtm::IllegalCell& e) {}
     
-    try { game1.addCharacter(GridPoint(0,0),  sniper1); }
+    try {
+        game1.addCharacter(GridPoint(0,0),  sniper1);
+        throw;
+    }
     catch (mtm::CellOccupied& e) {}
 
-    try { game1.move(GridPoint(0,0), GridPoint(3, 3)); }
+    try {
+        game1.move(GridPoint(0,0), GridPoint(3, 3));
+        throw;
+        }
     catch (mtm::MoveTooFar& e) {}
+
+
 
     cout << "[OK]" << endl;;
 }
@@ -96,4 +112,6 @@ int main ()
     exceptionTest();
     boardTest1(); // Private
     basicTest1(); // Private
+
+    //cout << ceil((double(5)/3));
 }
